d7d6fa6d5abb2cfa5ccd64a266b01df82969e1db
Mansi Dhamne <129254413+mansidhamne@users.noreply.github.com>
2025-02-26 12:10:41 +0530

    fixing

--- backend/routes/prescriptionRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { processPrescription } = require("../controllers/prescriptionController");
-const upload = require("../middleware/upload");
-const router = express.Router();
-
-router.post("/upload", upload.single("file"), processPrescription);
-
-module.exports = router;
